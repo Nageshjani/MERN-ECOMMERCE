786701a5da671ce330c0d36c9f509ca56eae4fe1
nagesh444 <nageshjani4@gmail.com>
2022-04-06 15:05:20 +0530

    kj

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -13,26 +13,9 @@ export const GlobalState = createContext()
 
 export const DataProvider = ({children}) =>{
     const [token, setToken] = useState(false)
-    useEffect(() =>{
-        const firstLogin = localStorage.getItem('firstLogin')
-        if(firstLogin){
-            const refreshToken = async () =>{
-                try{
-                    const res = await axios.get('user/refresh_token')
-                    setToken(res.data.accesstoken)
-                    console.log('token',res)
-                    setTimeout(() => {
-                        refreshToken()
-                    }, 10 * 60 * 1000)
-
-                }catch(err){
-                    alert(err.response.data.msg)
-
-                }
-            }
-            refreshToken()
-        }
-    },[])
+    const res = await axios.get('user/refresh_token')
+    setToken(res.data.accesstoken)
+    console.log('token',res)
 
 
     
