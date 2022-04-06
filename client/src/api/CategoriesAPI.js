f927eb85ae2ad029eae2d509d0ff544ce502bca2
nagesh444 <nageshjani4@gmail.com>
2022-04-06 14:03:18 +0530

    Kj

--- client/src/api/CategoriesAPI.js
+++ client/src/api/CategoriesAPI.js
@@ -1,10 +1,12 @@
 import {useState, useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 axios.defaults.withCredentials=true
 
 function CategoriesAPI() {
     const [categories, setCategories] = useState([])
     const [callback, setCallback] = useState(false)
+    const navigate=useNavigate()
 
     useEffect(() =>{
         
@@ -14,6 +16,7 @@ function CategoriesAPI() {
                 const res = await axios.get('https://mern-ecommerce-forall.herokuapp.com/api/category')
                 console.log(res)
                 setCategories(res.data)
+                navigate('/category')
 
             } catch(err){
                 window.location.href = "https://mern-full-auth.herokuapp.com";
