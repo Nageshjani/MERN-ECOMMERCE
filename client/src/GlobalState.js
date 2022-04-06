3c9b3c8bae411a4694a808ed5a1425a69bf4e1a6
nagesh444 <nageshjani4@gmail.com>
2022-04-06 15:23:55 +0530

    kj

--- client/src/GlobalState.js
+++ client/src/GlobalState.js
@@ -1,11 +1,8 @@
 import React, {createContext, useState, useEffect} from 'react'
-import ProductsAPI from './api/ProductsAPI'
-import UserApi from './api/UserApi'
-import CategoriesAPI from './api/CategoriesAPI'
-export const GlobalState = createContext()
 import axios from 'axios'
 axios.defaults.withCredentials=true
 
+export const GlobalState = createContext()
 
 export const DataProvider = ({children}) =>{
     const [token, setToken] = useState(false)
@@ -34,9 +31,7 @@ export const DataProvider = ({children}) =>{
     
     const state = {
         token: [token, setToken],
-        productsAPI: ProductsAPI(),
-        userAPI: UserApi(token),
-        categoriesAPI: CategoriesAPI()
+       
         
     }
 
